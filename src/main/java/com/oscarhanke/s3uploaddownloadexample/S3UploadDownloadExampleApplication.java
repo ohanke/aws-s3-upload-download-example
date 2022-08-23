5b3bbe75e56e8527538b5db3d45ff2fc0930714e
ohanke <steamoha@wp.pl>
2022-08-23 18:16:17 +0200

    Generated new project at start.spring.io

---
+++ src/main/java/com/oscarhanke/s3uploaddownloadexample/S3UploadDownloadExampleApplication.java
@@ -0,0 +1,13 @@
+package com.oscarhanke.s3uploaddownloadexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3UploadDownloadExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3UploadDownloadExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oscarhanke/s3uploaddownloadexample/S3UploadDownloadExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oscarhanke.s3uploaddownloadexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3UploadDownloadExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
